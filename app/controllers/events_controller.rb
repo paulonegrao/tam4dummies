374772138d710f99f301914d16b18a5ed3f1a1dd
Paulo Negrao <paulo.negrao@ymail.com>
2016-03-31 12:58:39 -0700

    adjusting streams#show layout & fadeIn tam_capturing button

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,23 @@ class EventsController < ApplicationController
   def update
     @event = Event.last
 
+    respond_to do |format|
+      attr_update = event_params
+      attr_update[:updated_at] = Time.now
+      if @event.update_attributes(attr_update)
+# t4d.s03 - TOUCH "tam_keep_alive"; live_id=0; live_yt_id=resquest_path
+        system('touch /home/pi/rails/tam4dummies/stream/tam_keep_alive &')
+        format.js { render :update_success }
+      else
+        format.js { render :update_error }
+      end
+    end
+  end
+
+# t4d.s03 - find last (and unic) Event and passes it to show.js.erb (on events view)
+  def show
+    @event = Event.last
+
     if running_rpi?
         broadcast_id_fn = `ls /home/pi/rails/tam4dummies/stream/create_broadcast/ | awk '{ print $NF }'`
     else
@@ -41,28 +58,11 @@ class EventsController < ApplicationController
         else
           `rm /Users/paulonegrao/codecore/railsdir/tam_for_dummies_app/stream/create_broadcast/*`
         end
+    else
+        @broadcast_id = nil
     end
 
     respond_to do |format|
-      attr_update = event_params
-      attr_update[:updated_at] = Time.now
-      if @event.update_attributes(attr_update)
-# t4d.s03 - TOUCH "tam_keep_alive"; live_id=0; live_yt_id=resquest_path
-        system('touch /home/pi/rails/tam4dummies/stream/tam_keep_alive &')
-	      format.html
-        format.js { render :update_success }
-      else
-        format.html
-        format.js { render :update_error }
-      end
-    end
-  end
-
-# t4d.s03 - find last (and unic) Event and passes it to show.js.erb (on events view)
-  def show
-    @event = Event.last
-    respond_to do |format|
-      format.html
       format.js
     end
   end
