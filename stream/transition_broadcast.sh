68dc789b6dd3003db8d1ac914509ecd8554d0f5a
Paulo Negrao <paulo.negrao@ymail.com>
2016-03-31 17:24:12 -0700

    SetTransition to JAVA & cut to stream#new http

--- stream/transition_broadcast.sh
+++ stream/transition_broadcast.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #EXECUTABLE='mvn -f /home/pi/ytlive/api-samples/java/pom.xml exec:java -Dexec.mainClass="com.google.api.services.samples.youtube.cmdline.live.SetTransition" -Dexec.args="'$1' '$2'"'
-EXECUTABLE='java -cp /home/pi/ytlive/api-samples/java/target/samples-0.0.1-SNAPSHOT.jar com.google.api.services.samples.youtube.cmdline.live.SetTransition '$1 $2
+# EXECUTABLE='java -cp /home/pi/ytlive/api-samples/java/target/samples-0.0.1-SNAPSHOT.jar com.google.api.services.samples.youtube.cmdline.live.SetTransition '$1 $2
+#
+# eval $EXECUTABLE
 
-eval $EXECUTABLE
+`java -cp /home/pi/ytlive/api-samples/java/target/samples-0.0.1-SNAPSHOT.jar com.google.api.services.samples.youtube.cmdline.live.SetTransition $1 $2`
