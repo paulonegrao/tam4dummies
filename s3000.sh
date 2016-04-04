07333855088b67684a100c08f5eb1affd5d310b2
Paulo Negrao <paulo.negrao@ymail.com>
2016-04-04 02:41:03 -0700

    iptables OUTPUT 80/3000; Pi_avatar.png

--- s3000.sh
+++ s3000.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 3000
 
 source ~/.rvm/scripts/rvm
 
