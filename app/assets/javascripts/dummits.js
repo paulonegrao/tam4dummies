53f57db8e52608c1ad2fcc6c80b5147565a00926
Paulo Negrao <paulo.negrao@ymail.com>
2016-04-06 16:52:02 -0700

    no onclicW for dummits

--- app/assets/javascripts/dummits.js
+++ app/assets/javascripts/dummits.js
@@ -4,12 +4,7 @@ $(document).on("page:change", function() {
           var tam_obj = $('#on-off-air');
           if (tam_obj.hasClass('on-air')) {
             // t4dcode: if "event_live_id" = "on-off-air data-live" & current page is = "on-off-air onclick" -> go DUMMIT; else alert msgs
-            if (tam_obj.attr('onclick').includes("window.location.href=")) {
-              var onclickW = tam_obj.attr('onclick').split("'")[1];
-            } else {
-              var onclickW =tam_obj.attr('onclick');
-            };
-            if ($('#event_live_id').attr('value') == tam_obj.attr('data-live') && window.location.href.includes(onclickW) {
+            if ($('#event_live_id').attr('value') == tam_obj.attr('data-live') && window.location.href.includes(tam_obj.attr('onclick'))) {
                 $("#dummit-new-1").addClass($("#dummit-grid-tail a").css("content").slice(1, -1));
                 $("#dummit-new-2").addClass($("#dummit-grid-tail a").css("content").slice(1, -1));
                 $("#dummit-new-3").addClass($("#dummit-grid-tail a").css("content").slice(1, -1));
